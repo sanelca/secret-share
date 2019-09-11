7eb0faa53421fba96b3c76781fc37a729a243d96
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-09-11 13:56:28 +0200

    Secret Share App has finished

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -6,6 +6,16 @@ class SecretsController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @secret = Secret.find_by(public_uid: params[:id])
+    if @secret
+      @secret.destroy
+    else
+      redirect_to root_path
+    end
+
+  end
+
   private
     def secret_params
       params.require(:secret).permit(:message)
